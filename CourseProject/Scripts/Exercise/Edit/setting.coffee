deacfad2d621c0478547c99d6fd702764c63c5a0
mam0ru <alex.truhan@mail.ru>
2014-08-12 21:15:17 +0300

    script setting.coffee edit

--- CourseProject/Scripts/Exercise/Edit/setting.coffee
+++ CourseProject/Scripts/Exercise/Edit/setting.coffee
@@ -1,5 +1,9 @@
 ﻿$ ->
   $(document).ready () ->
     $(".md-header").css('height', '35px')
-    $("[name='deleteTag']").on 'click', () ->
-      alert "cliked"
+    $("[name='deleteTag']").on 'click', (e) ->
+      e.stopPropagation()
+      e.preventDefault()
+      parent1 = $(this).parent()
+      parent2 = parent1.parent()
+      parent2.remove()
--- CourseProject/Scripts/Exercise/Edit/setting.js
+++ CourseProject/Scripts/Exercise/Edit/setting.js
@@ -2,8 +2,13 @@
   $(function() {
     return $(document).ready(function() {
       $(".md-header").css('height', '35px');
-      return $("[name='deleteTag']").on('click', function() {
-        return alert("cliked");
+      return $("[name='deleteTag']").on('click', function(e) {
+        var parent1, parent2;
+        e.stopPropagation();
+        e.preventDefault();
+        parent1 = $(this).parent();
+        parent2 = parent1.parent();
+        return parent2.remove();
       });
     });
   });
