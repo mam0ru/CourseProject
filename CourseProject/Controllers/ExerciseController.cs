c26631e0af44d6cfc40b11276913bdced4d3f8ed
MirNat <mire-natasha@yandex.ru>
2014-08-14 20:58:34 +0300

    front-end min changes beginning

--- CourseProject/Controllers/ExerciseController.cs
+++ CourseProject/Controllers/ExerciseController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.ServiceModel.Description;
+//using System.ServiceModel.Description;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
