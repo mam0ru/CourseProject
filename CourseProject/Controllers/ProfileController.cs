eec7b01d082f19cd951983870ad78638ce6c0a63
MirNat <mire-natasha@yandex.ru>
2014-08-19 13:45:45 +0300

    read mode final

--- CourseProject/Controllers/ProfileController.cs
+++ CourseProject/Controllers/ProfileController.cs
@@ -67,7 +67,7 @@ namespace CourseProject.Controllers
         public ActionResult Rating()
         {
             var users = UserManager.Users.OrderByDescending(user => user.RightAnswers.Count());
-            return View("Rating", users);//applicationUserRepository.Get().OrderBy(user => user.RightAnswers.Count()));
+            return View(users);//applicationUserRepository.Get().OrderBy(user => user.RightAnswers.Count()));
         }
         /*
        [HttpPost]
