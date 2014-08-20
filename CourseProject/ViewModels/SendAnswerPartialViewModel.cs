1bf81135c7e1d92f4db0b3fbbd4d5eee0c6771bc
mam0ru <alex.truhan@mail.ru>
2014-08-20 21:10:27 +0300

    send answer

---
+++ CourseProject/ViewModels/SendAnswerPartialViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CourseProject.ViewModels
+{
+    public class SendAnswerPartialViewModel
+    {
+        public int TaskId { get; set; }
+
+        public String Answer { get; set; }
+    }
+}
