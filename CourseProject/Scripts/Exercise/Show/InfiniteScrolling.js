0c3c7caea7706bc93b96eb696815c041e92e0f1a
mam0ru <alex.truhan@mail.ru>
2014-08-22 19:07:29 +0300

    views edit

---
+++ CourseProject/Scripts/Exercise/Show/InfiniteScrolling.js
@@ -0,0 +1,29 @@
+﻿(function() {
+  $(function() {
+    return $(document).ready(function() {
+      var NoMoreData, blockNumber, inProgress;
+      blockNumber = 2;
+      NoMoreData = false;
+      inProgress = false;
+      return $(window).scroll(function() {
+        if ($(window).scrollTop() === $(document).height() - $(window).height() && !NoMoreData && !inProgress) {
+          inProgress = true;
+          $("#loadingDiv").show();
+          return $.post("/Exercise/InfinateScroll", {
+            "blockNumber": blockNumber,
+            "id": $('#Id')[0].value
+          }, function(data) {
+            blockNumber = blockNumber + 1;
+            NoMoreData = data.NoMoreData;
+            $("#bookListDiv").append(data.HTMLString);
+            $("#loadingDiv").hide();
+            return inProgress = false;
+          });
+        }
+      });
+    });
+  });
+
+}).call(this);
+
+//# sourceMappingURL=InfiniteScrolling.js.map
